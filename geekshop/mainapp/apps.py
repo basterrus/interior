7bdede98307c6709404096801eb4188f5271cc40
baster <pnedoshivin@gmail.com>
2021-10-12 22:01:20 +0300

    Практическое задание урока 1. Создана структура сайта, прописаны УРЛы, перенос графики впапку статик

---
+++ geekshop/mainapp/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MainappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    # default_app_config = 'mainapp.BboardConfig'
+    name = 'mainapp'
