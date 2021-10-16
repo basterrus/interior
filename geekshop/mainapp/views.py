5f499e06db01c68b0e13db55818e12e76f276c2a
baster <pnedoshivin@gmail.com>
2021-10-16 15:47:48 +0300

    Практическое задание урока 2. Выполнено согласно пунктам ДЗ

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,81 @@
 from django.shortcuts import render
 
+links_menu = [
+    {
+        'url': 'products',
+        'title': 'Все'
+    },
+    {
+        'url': 'products_home',
+        'title': 'Дом'
+    },
+    {
+        'url': 'products_modern',
+        'title': 'Модерн'
+    },
+    {
+        'url': 'products_office',
+        'title': 'Оффис'
+    },
+    {
+        'url': 'products_classic',
+        'title': 'Классика'
+    }
+]
+
 
 # Create your views here.
 def main(request):
-    return render(request, 'mainapp/index.html')
-
-
-def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'links_menu': links_menu,
+        'title': 'Главная'
+    }
+    return render(request, 'mainapp/index.html', context=context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'links_menu': links_menu,
+        'title': 'Контакты'
+    }
+    return render(request, 'mainapp/contact.html', context=context)
+
+
+def products(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Продукты'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_home(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Продукты для дома'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_modern(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Продукты модерн'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_office(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Продукты для офиса'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_classic(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Продукты классика'
+    }
+    return render(request, 'mainapp/products.html', context=context)
