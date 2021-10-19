bf24187c469c0789ef580431504b025c5e6feb65
baster <pnedoshivin@gmail.com>
2021-10-19 20:00:01 +0300

    Практическое задание урока 3. Выполнено согласно пунктам ДЗ, по итогу с главной страницы выпал блок с популярными товарами. Найти у меня опыта не хватает, скорее всего опять с путями

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=64, unique=True)
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категория'
+        ordering = ('-id',)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=128, verbose_name='Наименование')
+    image = models.ImageField(upload_to='products', blank=True, verbose_name='Изображение')
+    short_desc = models.CharField(max_length=255, verbose_name='Краткое описание')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(decimal_places=2, max_digits=10, default=0, verbose_name='Цена')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
