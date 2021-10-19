dbeadb5e74d75f493c19a76218d9ae7e2cead7c5
baster <pnedoshivin@gmail.com>
2021-10-19 20:00:01 +0300

    Практическое задание урока 3. Выполнено согласно пунктам ДЗ, по итогу с главной страницы выпал блок с популярными товарами. Найти у меня опыта не хватает, скорее всего опять с путями

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,81 +1,25 @@
 from django.shortcuts import render
-
-links_menu = [
-    {
-        'url': 'products',
-        'title': 'Все'
-    },
-    {
-        'url': 'products_home',
-        'title': 'Дом'
-    },
-    {
-        'url': 'products_modern',
-        'title': 'Модерн'
-    },
-    {
-        'url': 'products_office',
-        'title': 'Оффис'
-    },
-    {
-        'url': 'products_classic',
-        'title': 'Классика'
-    }
-]
+from mainapp.models import Product, ProductCategory
 
 
-# Create your views here.
 def main(request):
     context = {
-        'links_menu': links_menu,
-        'title': 'Главная'
+        'title': 'Главная',
+        'links_menu': Product.objects.all()[:4],
     }
     return render(request, 'mainapp/index.html', context=context)
 
 
 def contact(request):
     context = {
-        'links_menu': links_menu,
-        'title': 'Контакты'
+        'title': 'Контакты',
     }
     return render(request, 'mainapp/contact.html', context=context)
 
 
-def products(request):
+def products(request, pk=None):
     context = {
-        'links_menu': links_menu,
+        'links_menu': ProductCategory.objects.all(),
         'title': 'Продукты'
-    }
-    return render(request, 'mainapp/products.html', context=context)
-
-
-def products_home(request):
-    context = {
-        'links_menu': links_menu,
-        'title': 'Продукты для дома'
-    }
-    return render(request, 'mainapp/products.html', context=context)
-
-
-def products_modern(request):
-    context = {
-        'links_menu': links_menu,
-        'title': 'Продукты модерн'
-    }
-    return render(request, 'mainapp/products.html', context=context)
-
-
-def products_office(request):
-    context = {
-        'links_menu': links_menu,
-        'title': 'Продукты для офиса'
-    }
-    return render(request, 'mainapp/products.html', context=context)
-
-
-def products_classic(request):
-    context = {
-        'links_menu': links_menu,
-        'title': 'Продукты классика'
     }
     return render(request, 'mainapp/products.html', context=context)
