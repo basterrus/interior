6ac04fbb92c45918f1fd3d1b1c831c769492704e
Pavel Nedoshivin <pnedoshivin@gmail.com>
2021-10-24 16:53:43 +0300

    Практическое задание к уроку 4. Сделано по пунктам дз, кроме звездочки

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from authapp.forms import UserLoginForm
+from authapp.forms import UserLoginForm, UserRegisterForm, ShopUserEditForm
 from django.contrib import auth
 from django.urls import reverse
 
@@ -9,17 +9,66 @@ def login(request):
 
     login_form = UserLoginForm(data=request.POST)
     if request.method == 'POST' and login_form.is_valid():
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
             return HttpResponseRedirect(reverse('main'))
-    content = {'title': title, 'login_form': login_form }
-    return render(request, 'authapp/login.html', content)
+
+    context = {
+        'title': title,
+        'login_form': login_form}
+
+    return render(request, 'authapp/login.html', context)
 
 
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('main'))
+
+
+def register(request):
+    title = 'Регистрация пользователя'
+
+    if request.method == 'POST':
+        register_form = UserRegisterForm(request.POST, request.FILES)
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('main'))
+    else:
+        register_form = UserRegisterForm()
+
+    context = {
+        'register_form': register_form,
+        'title': title,
+    }
+
+    return render(request, 'authapp/register.html', context)
+
+
+def edit(request):
+    title = 'Редактирование профиля'
+
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    context = {
+        'edit_form': edit_form,
+        'title': title,
+    }
+    print(request.user)
+    return render(request, 'authapp/edit.html', context)
+
+
+
+
+
+def delete(request):
+    return None
--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -5,13 +5,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import mainapp.views as mainapp
-import authapp.views as authapp
 
 urlpatterns = [
     path('', mainapp.main, name='main'),
     path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact, name='contact'),
-    path('login/', include('authapp.login', namespace='login')),
+    path('auth/', include('authapp.urls', namespace='auth')),
+    path('auth/', include('authapp.urls', namespace='register')),
     path('admin/', admin.site.urls),
 ]
 
