ada1e522feb4d796b5a93d8bfc9643f36bd32554
baster <pnedoshivin@gmail.com>
2021-10-24 09:40:55 +0300

    synchronize

---
+++ geekshop/authapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    avatar = models.ImageField(upload_to='avatar', blank=True, verbose_name='Аватар')
+    age = models.PositiveIntegerField(verbose_name='Возраст', blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователь'
+        # abstract = True
+
+
