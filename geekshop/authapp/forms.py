5977b94c02d3c4da74280200f6743ffebc7fb210
Pavel Nedoshivin <pnedoshivin@gmail.com>
2021-10-25 12:39:26 +0300

    Практическое задание к уроку 4. Сделано по пунктам дз, кроме звездочки. Пофиксен баг с формой регистрации

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
-
 from authapp.models import UserProfile
+import os
 
 
 class UserLoginForm(AuthenticationForm):
@@ -35,7 +35,6 @@ class UserRegisterForm(UserCreationForm):
 
 
 class ShopUserEditForm(UserChangeForm):
-
     class Meta:
         model = UserProfile
         fields = ('username', 'first_name', 'last_name', 'age', 'avatar', 'email', 'password')
