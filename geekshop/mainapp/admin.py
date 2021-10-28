c8240f18cb9b67f6844a822abb276eaf9650b76b
baster <pnedoshivin@gmail.com>
2021-10-28 14:50:03 +0300

    Добавил класс mainapp admin.py для цивильного отображения

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from mainapp.models import Product, ProductCategory
 
 
+class MaAdmin(admin.ModelAdmin):
+    list_display = ('category', 'name', 'short_desc', 'price', 'quantity')
+    list_display_links = ('name',)
+    search_fields = ('name', 'short_desc')
 
-admin.site.register(Product)
+
+admin.site.register(Product, MaAdmin)
+# admin.site.register(Product)
 admin.site.register(ProductCategory)
